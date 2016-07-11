4c54808f1186c9274d0938db06764679113bcba4
Alexandre Leblanc <info@alexandre-leblanc.com>
2016-07-11 14:57:25 -0400

    replace _delay_ms for delay

--- max6675.cpp
+++ max6675.cpp
@@ -6,7 +6,7 @@
 #elif defined(ESP8266)
   #include <pgmspace.h>
 #endif
-#include <util/delay.h>
+//#include <util/delay.h>
 #include <stdlib.h>
 #include "max6675.h"
 
@@ -17,7 +17,7 @@ MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) {
 
   //define pin modes
   pinMode(cs, OUTPUT);
-  pinMode(sclk, OUTPUT); 
+  pinMode(sclk, OUTPUT);
   pinMode(miso, INPUT);
 
   digitalWrite(cs, HIGH);
@@ -27,7 +27,7 @@ double MAX6675::readCelsius(void) {
   uint16_t v;
 
   digitalWrite(cs, LOW);
-  _delay_ms(1);
+  delay(1);
 
   v = spiread();
   v <<= 8;
@@ -37,7 +37,7 @@ double MAX6675::readCelsius(void) {
 
   if (v & 0x4) {
     // uh oh, no thermocouple attached!
-    return NAN; 
+    return NAN;
     //return -100;
   }
 
@@ -50,21 +50,21 @@ double MAX6675::readFahrenheit(void) {
   return readCelsius() * 9.0/5.0 + 32;
 }
 
-byte MAX6675::spiread(void) { 
+byte MAX6675::spiread(void) {
   int i;
   byte d = 0;
 
   for (i=7; i>=0; i--)
   {
     digitalWrite(sclk, LOW);
-    _delay_ms(1);
+    delay(1);
     if (digitalRead(miso)) {
       //set the bit to 0 no matter what
       d |= (1 << i);
     }
 
     digitalWrite(sclk, HIGH);
-    _delay_ms(1);
+    delay(1);
   }
 
   return d;
